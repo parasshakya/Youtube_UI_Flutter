45ca0cf3ca1169d37f148a99b5c4df407b3ac76a
Paras Shakya <parasshakya8085@gmail.com>
2021-06-26 13:25:31 +0545

    Created VideoScreen.dart

---
+++ lib/screens/VideoScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class VideoScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
