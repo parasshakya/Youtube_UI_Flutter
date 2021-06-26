2365b7de28edba34307948e2c5d2ec6adbd89797
Paras Shakya <parasshakya8085@gmail.com>
2021-06-26 12:20:04 +0545

    Added Miniplayer in the Stack

--- lib/screens/NavScreen.dart
+++ lib/screens/NavScreen.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miniplayer/miniplayer.dart';
 import 'package:youtubeuiflutter/screens/ImportScreens.dart';
 
 import '../data.dart';
+final selectedVideoProvider = StateProvider<Video?>((ref) => null);
 
 class NavScreen extends StatefulWidget {
-  final selectedVideoProvider = StateProvider<Video?>((ref) => null);
 
   @override
   _NavScreenState createState() => _NavScreenState();
 }
 
 class _NavScreenState extends State<NavScreen> {
+  double _playerMinHeight = 60.0;
   final _screens = [
     HomeScreen(),
     const Scaffold(body: Center(child: Text('Explore'),),),
@@ -23,12 +25,79 @@ class _NavScreenState extends State<NavScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children:
-          _screens.asMap().map((i,screen) => MapEntry(i, Offstage(
-            child: screen,
-            offstage: _selectedIndex != i,
-          ))).values.toList(),
+      body: Consumer(
+        builder: (context,watch,_){
+          final selectedVideo = watch(selectedVideoProvider).state;
+          print(selectedVideo);
+         return Stack(
+            children:
+            _screens.asMap().map((i,screen) => MapEntry(i, Offstage(
+              child: screen,
+              offstage: _selectedIndex != i,
+            ))).values.toList()..add(
+              Offstage(
+                offstage: selectedVideo == null ,
+                child: Miniplayer(
+                  minHeight: _playerMinHeight ,
+                  maxHeight: MediaQuery.of(context).size.height,
+                  builder: (height,percentage){
+                    return Container(
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Image.network(
+                                selectedVideo!.thumbnailUrl,
+                                height: _playerMinHeight - 4,
+                                width: 120.0,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(width: 8.0,),
+                              Expanded(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Flexible(child: Text(selectedVideo.title, style: Theme.of(context).textTheme.caption!.copyWith(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                      overflow: TextOverflow.ellipsis,
+                                    )),
+                                    Text('${selectedVideo.author.username}',
+                                      style: Theme.of(context).textTheme.caption!.copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      ),)
+                                  ],
+                                ),
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.play_arrow),
+                                onPressed: (){},
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.close),
+                                onPressed: (){
+                                  context.read(selectedVideoProvider).state = null;
+                                },
+                              )
+                            ],
+                          ),
+                          const LinearProgressIndicator(
+                            value: 0.6,
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              )
+            ),
+
+          );
+        },
 
       ),
       bottomNavigationBar: BottomNavigationBar(
